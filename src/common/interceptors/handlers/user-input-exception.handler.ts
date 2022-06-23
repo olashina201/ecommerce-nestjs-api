c3f9af77f947697b773c37295f4b42cf9e017bd2
olashina201 <olashina201@gmail.com>
2022-06-23 19:43:42 +0100

    fix error

--- src/common/interceptors/handlers/user-input-exception.handler.ts
+++ src/common/interceptors/handlers/user-input-exception.handler.ts
@@ -4,9 +4,9 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthServiceInputException } from 'src/auth/exceptions/auth-service-input.exception';
-import { ProductServiceInputException } from 'src/models/product/exceptions/product-service-input.exception';
-import { PurchaseServiceInputException } from 'src/models/purchase/exceptions/purchase-service-input.exception';
-import { UserServiceInputException } from 'src/models/user/exceptions/user-service-input.exception';
+import { ProductServiceInputException } from 'src/product/exceptions/product-service-input.exception';
+import { PurchaseServiceInputException } from 'src/purchase/exceptions/purchase-service-input.exception';
+import { UserServiceInputException } from 'src/user/exceptions/user-service-input.exception';
 import { ExceptionHandler } from './exception.handler';
 
 /** Catches user input errors and throws the

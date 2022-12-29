57ac6b917213ce258fb1b9f99a7e76ba2320b25a
olashina201 <olashina201@gmail.com>
2022-12-29 07:55:41 +0100

    update

--- src/common/decorators/is-admin.decorator.ts
+++ src/common/decorators/is-admin.decorator.ts
@@ -6,16 +6,8 @@ import {
 } from '@nestjs/swagger';
 import { RolesGuard } from '../guards/roles.guard';
 
-/** Value to verify if @IsAdmin() is being used in the endpoint
- *
- * Consult <a href="../injectables/RolesGuard.html">RolesGuard</a>
- */
 export const IS_ADMIN_KEY = 'isAdmin';
 
-/** Makes an endpoint accessible only by admin users
- *
- * <br>Example: Create, update and remove products
- */
 export function IsAdmin(): <TFunction>(
   target: object | TFunction,
   propertyKey?: string | symbol,
